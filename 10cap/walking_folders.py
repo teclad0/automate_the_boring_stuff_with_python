383cf83fbc0b51c5b767f4630a0b199c7b566e8e
teclad0 <hemteo@outlook.com>
2020-11-14 21:56:59 -0300

    changed the directories names, now for real

--- 10cap/walking_folders.py
+++
@@ -1,8 +0,0 @@
-import os
-for folderName, subfolders, filenames in os.walk('/home/hem/Documents/py'):
-    print('The current folder is ' + folderName)
-    for subfolder in subfolders:
-        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
-    for filename in filenames:
-        print('FILE INSIDE ' + folderName + ': ' + filename)
-    print('')
--- 12cap/localweather.py
+++
@@ -1,5 +0,0 @@
-#! python3
-import webbrowser
-webbrowser.open('https://www.climatempo.com.br/previsao-do-tempo/cidade/405/sp-sp')
-
-
